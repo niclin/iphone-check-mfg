861f2942db232e6dd7ef4f7a3001a782ed3d21d2
niclin <bboyceo@hotmail.com>
2016-11-25 01:45:52 +0800

    Fix bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,6 @@
 //= require turbolinks
 //= require_tree .
 
-<div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
